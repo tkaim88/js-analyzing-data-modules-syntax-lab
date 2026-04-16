110ea2189874866fda4c852943355607174a527d
Your GitHub Username <your-email@example.com>
2026-04-16 08:48:33 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
+require('datejs');
+// Step 1: Create a combineUsers function - Create a function named combineUsers and assign the following arguments:
+function combineUsers(...args) {
+
+// Step 2: Initialize the Return Object
+  const combinedObject = {
+    users: []
+  };
+
+// Step 3: Loop through args to isolate each array and merge them
+for (let i = 0; i < args.length; i++) {
+  combinedObject.users.push(...args[i]);
+}
+
+// Step 4: Get todays update. add an attribute to combined object called merge_date and using datejs give it the current date
+  const today = new Date();
+  combinedObject.merge_date = today.toString("M/d/yyyy");
+  
+// Return the combinedObject
+return combinedObject;
+}
 
 
 
 module.exports = {
-  ...(typeof combineUsers !== 'undefined' && { combineUsers })
+  combineUsers
 };
